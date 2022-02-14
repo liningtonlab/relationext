e7c2ff3b62415f6c72d8723c2d5d66dc32b660f1
Ben <benledingham1@gmail.com>
2022-02-14 22:13:02 +0000

    pre-release

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ requirements = [
 setuptools.setup(
     include_package_data=True,
     name='relationext',
-    version='0.2.5',
+    version='0.2.7',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
