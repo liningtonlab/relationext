460bc86ff3a6567fe09e5a04f7b00892f0a725fc
Ben <benledingham1@gmail.com>
2022-02-14 22:56:11 +0000

    pre-release

--- relationext/organism_categorizer.py
+++ relationext/organism_categorizer.py
@@ -12,7 +12,7 @@ def read_text_file(filename):
 
 
 # Organism lists used in match_org(user).
-micro_list = read_text_file(pkr.resource_stream(__name__, "data/microbe_genera_list"))
+micro_list = read_text_file(pkr.resource_stream(__name__, "data/microbe_genera_list.txt"))
 plant_list = read_text_file("data/plant_genera_list.txt")
 animal_list = read_text_file("data/animal_genera_list.txt")
 pathogen_list = read_text_file("data/pathogen_genera_list.txt")
