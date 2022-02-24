cb454272168390b1ac4b0333d519a842e38803d3
Ben <benledingham1@gmail.com>
2022-02-24 19:27:23 +0000

    nltk installations

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ requirements = [
 setuptools.setup(
     include_package_data=True,
     name='relationext',
-    version='0.6.0',
+    version='0.7.0',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
