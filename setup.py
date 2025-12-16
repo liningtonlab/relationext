97d4e484e205dc6aafdd0bacdd2bf8f4cf149372
Matthew Pin <MattTPin@gmail.com>
2025-12-16 13:01:42 -0800

    updated version

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ requirements = [
 setuptools.setup(
     include_package_data=True,
     name='relationext',
-    version='0.11.0',
+    version='0.12.0',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
